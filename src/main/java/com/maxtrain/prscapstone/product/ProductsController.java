63284f5a798219effa274f47d0a018fc3f108ebf
Charlie Ford <charles.ford1978@gmail.com>
2021-10-26 12:28:12 -0400

    Initial commit

---
+++ src/main/java/com/maxtrain/prscapstone/product/ProductsController.java
@@ -0,0 +1,15 @@
+package com.maxtrain.prscapstone.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+
+	@Autowired
+	private ProductRepository prodRepo;
+}
