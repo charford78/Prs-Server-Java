9c3621243a687607cad8868faab7d08efd3c26da
Charlie Ford <charles.ford1978@gmail.com>
2021-10-26 14:06:34 -0400

    Initial commit

---
+++ src/main/java/com/maxtrain/prscapstone/request/RequestRepository.java
@@ -0,0 +1,7 @@
+package com.maxtrain.prscapstone.request;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestRepository extends JpaRepository<Request, Integer>{
+
+}
---
+++ src/main/java/com/maxtrain/prscapstone/request/RequestsController.java
@@ -0,0 +1,15 @@
+package com.maxtrain.prscapstone.request;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/requests")
+public class RequestsController {
+	
+	@Autowired
+	private RequestRepository reqRepo;
+}
